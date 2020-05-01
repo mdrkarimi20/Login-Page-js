c08bfa1860c672544d01a4f025169fcd7cea23ed
mdrkarimi20 <mdrkarimi20@gmail.com>
2020-05-01 06:17:11 +0430

    rightSide

--- js/main.js
+++ js/main.js
@@ -25,18 +25,22 @@ let input3 = VueJS.createElement("input", {
 let iTh = VueJS.createElement("i", { class: "fas fa-th" });
 let imgSecurityRandom = VueJS.createElement("img", { src: "./img/download.png" });
 let iSync = VueJS.createElement("i", { class: "fas fa-sync" });
-let securityRandom = VueJS.createElement("i", { class: "security-random" }, [imgSecurityRandom, iSync]);
+let securityRandom = VueJS.createElement("div", { class: "security-random" }, [imgSecurityRandom, iSync]);
 let input4 = VueJS.createElement("input", {
     type: "submit",
     value: "ورود"
 });
-let form = VueJS.createElement("form", { method: "post", action: "http://your-web-domain.com/cgi-bin/formmail.cgi" }, [i1, input1, iUser, i2, input2, iLock, i3, input3, iTh, securityRandom, input4]);
+let input5 = VueJS.createElement("input", { type: "checkbox" });
+let label = VueJS.createElement("label", {}, ["مرا به خاطر بسپار", input5]);
+let remember = VueJS.createElement("div", { class: "remember" }, [label]);
+let form = VueJS.createElement("form", { method: "post", action: "http://your-web-domain.com/cgi-bin/formmail.cgi" }, [i1, input1, iUser, i2, input2, iLock, i3, input3, iTh, securityRandom, input4, remember]);
+
+let rightSide = VueJS.createElement("div", { className: "right-side" }, [h1, h3, h4, form]);
 
 
 
+let Multi = VueJS.createElement("h3", {}, "Multi Anti-Virus & Cybersecurity");
+let leftSide = VueJS.createElement("div", { className: "left-side" }, [Multi]);
 
-
-
-const main = VueJS.createElement("div", { className: "right-side" }, [h1, h3, h4, form]);
-
-VueJS.export("container", main);
+const main = VueJS.createElement("div", { className: "container" }, [rightSide, leftSide]);
+VueJS.export("root", main);
